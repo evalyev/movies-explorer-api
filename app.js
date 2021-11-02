7216641ab94f2e520de9a1e6691f5734b9c87093
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-11-02 20:41:07 +0300

    feat: added cors

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const checkErrors = require('./middlewares/check-errors');
-const { requestLogger, errorLogger } = require('./middlewares/logger'); 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { options } = require('./utils/constants');
 
 const { login, createUser } = require('./controllers/users');
 
@@ -20,13 +22,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 }).then(() => {
   console.log('ПОДКЛЮЧИЛИСЬ К БД');
 }).catch((res) => {
   console.log(`Ошибка: ${res.message}`);
 });
+
+app.use(cors(options));
 
 app.use(requestLogger);
 
