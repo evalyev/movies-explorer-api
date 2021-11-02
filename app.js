14cd998a717ded3aeec927d8d621c9e504c49661
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-11-02 18:09:55 +0300

    feat: Создана схема user

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('ПОДКЛЮЧИЛИСЬ К БД');
+}).catch((res) => {
+  console.log(`Ошибка: ${res.message}`);
+});
+
+
+
+
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
