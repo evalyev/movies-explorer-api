61775cd9ebb4024fee6262fcad74e4355cd354a6
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-11-04 01:18:57 +0300

    fix: updateUser

--- controllers/users.js
+++ controllers/users.js
@@ -17,12 +17,12 @@ module.exports.updateUser = (req, res, next) => {
     .then((user) => {
       if (!user || user._id.equals(req.user._id)) {
         User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
-          .then((thisUser) => checkQueryOfNull(thisUser, req, res))
-          .catch((err) => next(err));
       } else {
         next(new ConflictError('Error. Conflict request'));
       }
-    });
+    })
+    .then((thisUser) => checkQueryOfNull(thisUser, req, res))
+    .catch((err) => next(err));
 };
 
 module.exports.createUser = (req, res, next) => {
