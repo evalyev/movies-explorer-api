4bebeeae68e6f1eacbd778b4173bfa64dd600a91
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-11-04 23:13:17 +0300

    Адрес базы данных из env файла

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth');
 const checkErrors = require('./middlewares/check-errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { options } = require('./utils/constants');
+
+const { NODE_ENV, MONGODB } = process.env;
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect(`mongodb://localhost:27017/${NODE_ENV === 'production' ? MONGODB : 'moviesdb'}`, {
   useNewUrlParser: true,
 }).then(() => {
   console.log('ПОДКЛЮЧИЛИСЬ К БД');
