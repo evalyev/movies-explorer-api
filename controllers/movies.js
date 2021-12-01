78430de057423dbaeec1fb69ce3f7666fbf2af97
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-12-01 17:06:48 +0300

    удалил все фильмы

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,6 +7,7 @@ const ForbiddenError = require('../errors/forbidden-err');
 
 module.exports.getMyFilms = (req, res, next) => {
   const owner = req.user._id;
+  Movie.remove({});
 
   Movie.find({ owner })
     .then((movies) => checkQueryOfNull(movies, req, res, next))
